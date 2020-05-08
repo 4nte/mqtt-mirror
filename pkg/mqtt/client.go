10475d632cf96aff58c05c9b22efcda6ba2ddd17
sajben94 <sajben94@gmail.com>
2020-05-08 15:37:51 +0200

    lost_connection

--- pkg/mqtt/client.go
+++ pkg/mqtt/client.go
@@ -2,11 +2,13 @@ package mqtt
 
 import (
 	"fmt"
-	paho "github.com/eclipse/paho.mqtt.golang"
 	"time"
+
+	paho "github.com/eclipse/paho.mqtt.golang"
 )
 
 func NewClient(broker string, username string, password string, isSource bool) paho.Client {
+	var lost_connection = false
 	var alias string
 	if isSource {
 		alias = "source"
@@ -19,10 +21,14 @@ func NewClient(broker string, username string, password string, isSource bool) p
 
 	clientOpts.SetOnConnectHandler(func(client paho.Client) {
 		fmt.Printf("connection established to %s (%s)\n", broker, alias)
+		if lost_connection {
+			fmt.Printf("after lost_connection %s (%s)\n", broker, alias)
+		}
 		// TODO: channel
 	})
 	clientOpts.SetConnectionLostHandler(func(i paho.Client, error error) {
 		fmt.Print(fmt.Errorf("connection lost with %s (%s)", broker, alias))
+		lost_connection = true
 		// TODO: channel
 	})
 
